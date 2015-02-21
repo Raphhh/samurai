a9d98cf78f9fad4067148857575e0cdb6e08c1ad
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2015-02-21 22:57:04 +0100

    create DirectoryPathQuestion

---
+++ tests/Composer/Question/DirectoryPathQuestionTest.php
@@ -0,0 +1,81 @@
+<?php
+namespace Samurai\Composer\Question;
+
+use Pimple\Container;
+use Samurai\Composer\Composer;
+use Samurai\Composer\Project;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\BufferedOutput;
+use TRex\Cli\Executor;
+
+/**
+ * Class DirectoryPathQuestionTest
+ * @package Samurai\Composer\Question
+ * @author Raphaël Lefebvre <raphael@raphaellefebvre.be>
+ */
+class DirectoryPathQuestionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testExecuteWithDefaultName()
+    {
+        $input = $this->provideInput([]);
+        $output = new BufferedOutput();
+        $services = $this->provideServices();
+        $services['composer']->getproject()->setName('vendor/package');
+
+        $question = new DirectoryPathQuestion($services);
+        $this->assertTrue($question->execute($input, $output));
+        $this->assertSame('vendor/package', $services['composer']->getproject()->getDirectoryPath());
+    }
+
+    public function testExecuteValid()
+    {
+        $input = $this->provideInput(['--dir' => 'dir/path']);
+        $output = new BufferedOutput();
+        $services = $this->provideServices();
+        $services['composer']->getproject()->setName('vendor/package');
+
+        $question = new DirectoryPathQuestion($services);
+        $this->assertTrue($question->execute($input, $output));
+        $this->assertSame('dir/path', $services['composer']->getproject()->getDirectoryPath());
+    }
+
+    public function testExecuteEmpty()
+    {
+        $input = $this->provideInput([]);
+        $output = new BufferedOutput();
+        $services = $this->provideServices();
+
+        $question = new DirectoryPathQuestion($services);
+        $this->assertFalse($question->execute($input, $output));
+        $this->assertSame('', $services['composer']->getproject()->getDirectoryPath());
+    }
+
+    /**
+     * @return Container
+     */
+    private function provideServices()
+    {
+        $services = new Container();
+        $services['composer'] = function () {
+            return new Composer(new Project(), new Executor());
+        };
+        return $services;
+    }
+
+    /**
+     * @param array $args
+     * @return ArrayInput
+     */
+    private function provideInput(array $args)
+    {
+        return new ArrayInput(
+            $args,
+            new InputDefinition([
+                new InputOption('dir'),
+            ])
+        );
+    }
+}
+
