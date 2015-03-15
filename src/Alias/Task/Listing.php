6003582b2351f7ae67c7b1960714723580cd298e
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2015-03-15 23:35:28 +0100

    create Listing

---
+++ src/Alias/Task/Listing.php
@@ -0,0 +1,45 @@
+<?php
+namespace Samurai\Alias\Task;
+
+use Samurai\Task\Task;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class Listing
+ * @package Samurai\Alias\Task
+ * @author Raphaël Lefebvre <raphael@raphaellefebvre.be>
+ */
+class Listing extends Task
+{
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return bool
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        if($input->getOption('global')){
+            $output->writeln($this->mapAlias($this->getService('alias_manager')->getGlobal()));
+        }elseif($input->getOption('local')) {
+            $output->writeln($this->mapAlias($this->getService('alias_manager')->getLocal()));
+        }else{
+            $output->writeln($this->mapAlias($this->getService('alias_manager')->getAll()));
+        }
+        return true;
+    }
+
+    /**
+     * @param array $aliasList
+     * @return string
+     */
+    private function mapAlias(array $aliasList)
+    {
+        $result = '';
+        foreach($aliasList as $name => $alias){
+            $result .= '[' . $name . '] ' . $alias . "\n";
+        }
+        return trim($result);
+    }
+}
---
+++ tests/Alias/Task/ListingTest.php
@@ -0,0 +1,160 @@
+<?php
+namespace Samurai\Alias\Task;
+
+use Pimple\Container;
+use Samurai\Alias\Alias;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+/**
+ * Class ListingTest
+ * @package Samurai\Alias\Task
+ * @author Raphaël Lefebvre <raphael@raphaellefebvre.be>
+ */
+class ListingTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testExecuteWithGlobal()
+    {
+        $args = [
+            '--global' => '1',
+            'name' => 'name',
+            'description' => 'description',
+            'bootstrap' => 'bootstrap',
+            'version' => 'version',
+        ];
+        $input = $this->provideInput($args);
+        $output = new BufferedOutput();
+
+        $saving = new Listing(
+            $this->provideServices(
+                $this->provideAlias($args),
+                'getGlobal'
+            )
+        );
+        $this->assertTrue($saving->execute($input, $output));
+        $this->assertSame("[name] description (bootstrap version)\n", $output->fetch());
+    }
+
+    public function testExecuteWithLocal()
+    {
+        $args = [
+            '--local' => '1',
+            'name' => 'name',
+            'description' => 'description',
+            'bootstrap' => 'bootstrap',
+            'version' => 'version',
+        ];
+        $input = $this->provideInput($args);
+        $output = new BufferedOutput();
+
+        $saving = new Listing(
+            $this->provideServices(
+                $this->provideAlias($args),
+                'getLocal'
+            )
+        );
+        $this->assertTrue($saving->execute($input, $output));
+        $this->assertSame("[name] description (bootstrap version)\n", $output->fetch());
+    }
+
+    public function testExecuteWithAll()
+    {
+        $args = [
+            'name' => 'name',
+            'description' => 'description',
+            'bootstrap' => 'bootstrap',
+            'version' => 'version',
+        ];
+        $input = $this->provideInput($args);
+        $output = new BufferedOutput();
+
+        $saving = new Listing(
+            $this->provideServices(
+                $this->provideAlias($args),
+                'getAll'
+            )
+        );
+        $this->assertTrue($saving->execute($input, $output));
+        $this->assertSame("[name] description (bootstrap version)\n", $output->fetch());
+    }
+
+    /**
+     * @param array $args
+     * @return ArrayInput
+     */
+    private function provideInput(array $args)
+    {
+        return new ArrayInput(
+            $args,
+            new InputDefinition([
+                new InputOption('global'),
+                new InputOption('local'),
+                new InputArgument('name'),
+                new InputArgument('description'),
+                new InputArgument('bootstrap'),
+                new InputArgument('version'),
+            ])
+        );
+    }
+
+    /**
+     * @param array $args
+     * @return Alias
+     */
+    private function provideAlias(array $args)
+    {
+        $alias = new Alias();
+        $alias->setName($args['name']);
+        $alias->setDescription($args['description']);
+        $alias->setBootstrap($args['bootstrap']);
+        $alias->setVersion($args['version']);
+        return $alias;
+    }
+
+    /**
+     * @param Alias $alias
+     * @param string $useMethod
+     * @return Container
+     */
+    private function provideServices(Alias $alias, $useMethod)
+    {
+        $services = new Container();
+
+        $aliasManager = $this->provideAliasManager($alias, $useMethod);
+        $services['alias_manager'] = function () use($aliasManager){
+            return $aliasManager;
+        };
+
+        return $services;
+    }
+
+    /**
+     * @param Alias $alias
+     * @param string $useMethod
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function provideAliasManager(Alias $alias, $useMethod)
+    {
+        $aliasManager = $this->getMockBuilder('Samurai\Alias\AliasManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $aliasManager->expects($this->exactly((int) ($useMethod === 'getGlobal')))
+            ->method('getGlobal')
+            ->will($this->returnValue([$alias->getName() => $alias]));
+
+        $aliasManager->expects($this->exactly((int) ($useMethod === 'getLocal')))
+            ->method('getLocal')
+            ->will($this->returnValue([$alias->getName() => $alias]));
+
+        $aliasManager->expects($this->exactly((int) ($useMethod === 'getAll')))
+            ->method('getAll')
+            ->will($this->returnValue([$alias->getName() => $alias]));
+
+        return $aliasManager;
+    }
+}
