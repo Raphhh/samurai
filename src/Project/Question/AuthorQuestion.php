891715672584155fea5ec973690ff45d4b665fae
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2015-04-04 21:38:42 +0200

    create AuthorQuestion

---
+++ src/Project/Question/AuthorQuestion.php
@@ -0,0 +1,145 @@
+<?php
+namespace Samurai\Project\Question;
+
+use Samurai\Project\Author;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Question\Question as SimpleQuestion;
+
+/**
+ * Class AuthorQuestion
+ * @package Samurai\Project\Question
+ * @author Raphaël Lefebvre <raphael@raphaellefebvre.be>
+ */
+class AuthorQuestion extends Question
+{
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return bool
+     * @throws \Exception
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->setInitialAuthor($input, $output);
+        $this->setAdditionalAuthors($input, $output);
+        return (bool) $this->getProject()->getAuthors()->count();
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    private function setInitialAuthor(InputInterface $input, OutputInterface $output)
+    {
+        try {
+            $this->setAuthorFromGit($input, $output);
+        } catch (\Exception $e) {
+            $this->setAuthorFromQuestion($input, $output);
+        }
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    private function setAuthorFromGit(InputInterface $input, OutputInterface $output)
+    {
+        $author = new Author($this->retrieveGitUser());
+        if ($this->confirmAuthor($input, $output, $author)) {
+            $this->getProject()->addAuthor($author);
+        } else {
+            $this->setAuthorFromQuestion($input, $output);
+        }
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    private function setAuthorFromQuestion(InputInterface $input, OutputInterface $output)
+    {
+        $this->getProject()->addAuthor(
+            $this->ask(
+                $input,
+                $output,
+                $this->buildAuthorQuestion()
+            )
+        );
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    private function setAdditionalAuthors(InputInterface $input, OutputInterface $output)
+    {
+        while ($this->confirmAdditionalAuthor($input, $output)) {
+            $this->setAuthorFromQuestion($input, $output);
+        }
+    }
+
+    /**
+     * @return SimpleQuestion
+     */
+    private function buildAuthorQuestion()
+    {
+        $question = new SimpleQuestion('<question>Enter the author (name <mail@mail.com>):</question>');
+        $question->setValidator($this->buildValidator());
+        $question->setMaxAttempts(3);
+
+        return $question;
+    }
+
+    /**
+     * @return callable
+     */
+    private function buildValidator()
+    {
+        return function ($answer) {
+            return new Author($answer);
+        };
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return mixed
+     */
+    private function confirmAdditionalAuthor(InputInterface $input, OutputInterface $output)
+    {
+        return $this->ask(
+            $input,
+            $output,
+            new ConfirmationQuestion('<question>Do you want to add another author?[n]</question>', false)
+        );
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @param Author $author
+     * @return mixed
+     */
+    private function confirmAuthor(InputInterface $input, OutputInterface $output, Author $author)
+    {
+        return $this->ask(
+            $input,
+            $output,
+            new ConfirmationQuestion('<question>Do you confirm this author "'.$author.'"?[y]</question>')
+        );
+    }
+
+    /**
+     * @return string
+     */
+    private function retrieveGitUser()
+    {
+        $config = $this->getService('git')->config();
+        if(isset($config['user.name']) && isset($config['user.email'])){
+            return $config['user.name'] . ' <' . $config['user.email'] . '>';
+        }
+        return '';
+    }
+}
---
+++ tests/Project/Question/AuthorQuestionTest.php
@@ -0,0 +1,294 @@
+<?php
+namespace Samurai\Project\Question;
+
+use Pimple\Container;
+use Samurai\Project\Author;
+use Samurai\Project\Composer\Composer;
+use Samurai\Project\Project;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use TRex\Cli\Executor;
+
+/**
+ * Class AuthorQuestionTest
+ * @package Samurai\Project\Question
+ * @author Raphaël Lefebvre <raphael@raphaellefebvre.be>
+ */
+class AuthorQuestionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testExecuteForOneAuthorWithGit()
+    {
+        $input = new ArrayInput([]);
+        $output = new BufferedOutput();
+        $services = $this->provideServicesForOneAuthorWithGit();
+
+        $question = new AuthorQuestion($services);
+        $this->assertTrue($question->execute($input, $output));
+        $authors = $services['composer']->getproject()->getAuthors();
+        $this->assertCount(1, $authors);
+        $this->assertSame('git.name', $authors[0]->getName());
+        $this->assertSame('git.email@mail.com', $authors[0]->getEmail());
+    }
+
+    /**
+     * @return Container
+     */
+    private function provideServicesForOneAuthorWithGit()
+    {
+        $services = new Container();
+        $services['composer'] = function () {
+            return new Composer(new Project(), new Executor());
+        };
+
+        $git = $this->getMock('PHPGit\Git', array('config'));
+        $git->expects($this->once())
+            ->method('config')
+            ->will($this->returnValue([
+                'user.name' => 'git.name',
+                'user.email' => 'git.email@mail.com',
+            ]));
+
+
+        $services['git'] = function () use($git){
+            return $git;
+        };
+
+        $questionHelper = $this->getMock('Symfony\Component\Console\Helper\QuestionHelper', array('ask'));
+        $questionHelper->expects($this->at(0))
+            ->method('ask')
+            ->will(
+                $this->returnCallback(
+                    \Closure::bind(
+                        function(InputInterface $input, OutputInterface $output, Question $question){
+                            $this->assertSame(
+                                '<question>Do you confirm this author "git.name <git.email@mail.com>"?[y]</question>',
+                                $question->getQuestion()
+                            );
+                            return true;
+                        },
+                        $this
+                    )
+                )
+            );
+
+        $questionHelper->expects($this->at(1))
+            ->method('ask')
+            ->will(
+                $this->returnCallback(
+                    \Closure::bind(
+                        function(InputInterface $input, OutputInterface $output, Question $question){
+                            $this->assertSame(
+                                '<question>Do you want to add another author?[n]</question>',
+                                $question->getQuestion()
+                            );
+                            return false;
+                        },
+                        $this
+                    )
+                )
+            );
+
+        $services['question'] = function () use ($questionHelper){
+            return $questionHelper;
+        };
+
+        return $services;
+    }
+
+    public function testExecuteForTwoAuthorsWithGit()
+    {
+        $input = new ArrayInput([]);
+        $output = new BufferedOutput();
+        $services = $this->provideServicesForTwoAuthorsWithGit();
+
+        $question = new AuthorQuestion($services);
+        $this->assertTrue($question->execute($input, $output));
+        $authors = $services['composer']->getproject()->getAuthors();
+        $this->assertCount(2, $authors);
+        $this->assertSame('git.name', $authors[0]->getName());
+        $this->assertSame('git.email@mail.com', $authors[0]->getEmail());
+        $this->assertSame('add.name', $authors[1]->getName());
+        $this->assertSame('add.email@mail.com', $authors[1]->getEmail());
+    }
+
+    /**
+     * @return Container
+     */
+    private function provideServicesForTwoAuthorsWithGit()
+    {
+        $services = new Container();
+        $services['composer'] = function () {
+            return new Composer(new Project(), new Executor());
+        };
+
+        $git = $this->getMock('PHPGit\Git', array('config'));
+        $git->expects($this->once())
+            ->method('config')
+            ->will($this->returnValue([
+                'user.name' => 'git.name',
+                'user.email' => 'git.email@mail.com',
+            ]));
+
+
+        $services['git'] = function () use($git){
+            return $git;
+        };
+
+        $questionHelper = $this->getMock('Symfony\Component\Console\Helper\QuestionHelper', array('ask'));
+        $questionHelper->expects($this->at(0))
+            ->method('ask')
+            ->will(
+                $this->returnCallback(
+                    \Closure::bind(
+                        function(InputInterface $input, OutputInterface $output, Question $question){
+                            $this->assertSame(
+                                '<question>Do you confirm this author "git.name <git.email@mail.com>"?[y]</question>',
+                                $question->getQuestion()
+                            );
+                            return true;
+                        },
+                        $this
+                    )
+                )
+            );
+
+        $questionHelper->expects($this->at(1))
+            ->method('ask')
+            ->will(
+                $this->returnCallback(
+                    \Closure::bind(
+                        function(InputInterface $input, OutputInterface $output, Question $question){
+                            $this->assertSame(
+                                '<question>Do you want to add another author?[n]</question>',
+                                $question->getQuestion()
+                            );
+                            return true;
+                        },
+                        $this
+                    )
+                )
+            );
+
+        $questionHelper->expects($this->at(2))
+            ->method('ask')
+            ->will(
+                $this->returnCallback(
+                    \Closure::bind(
+                        function(InputInterface $input, OutputInterface $output, Question $question){
+                            $this->assertSame(
+                                '<question>Enter the author (name <mail@mail.com>):</question>',
+                                $question->getQuestion()
+                            );
+                            return new Author('add.name <add.email@mail.com>');
+                        },
+                        $this
+                    )
+                )
+            );
+
+        $services['question'] = function () use ($questionHelper){
+            return $questionHelper;
+        };
+
+        return $services;
+    }
+
+    public function testExecuteForTwoAuthorsWithoutGit()
+    {
+        $input = new ArrayInput([]);
+        $output = new BufferedOutput();
+        $services = $this->provideServicesForTwoAuthorsWithoutGit();
+
+        $question = new AuthorQuestion($services);
+        $this->assertTrue($question->execute($input, $output));
+        $authors = $services['composer']->getproject()->getAuthors();
+        $this->assertCount(2, $authors);
+        $this->assertSame('main.name', $authors[0]->getName());
+        $this->assertSame('main.email@mail.com', $authors[0]->getEmail());
+        $this->assertSame('add.name', $authors[1]->getName());
+        $this->assertSame('add.email@mail.com', $authors[1]->getEmail());
+    }
+
+    /**
+     * @return Container
+     */
+    private function provideServicesForTwoAuthorsWithoutGit()
+    {
+        $services = new Container();
+        $services['composer'] = function () {
+            return new Composer(new Project(), new Executor());
+        };
+
+        $git = $this->getMock('PHPGit\Git', array('config'));
+        $git->expects($this->once())
+            ->method('config')
+            ->will($this->returnValue([]));
+
+
+        $services['git'] = function () use($git){
+            return $git;
+        };
+
+        $questionHelper = $this->getMock('Symfony\Component\Console\Helper\QuestionHelper', array('ask'));
+        $questionHelper->expects($this->at(0))
+            ->method('ask')
+            ->will(
+                $this->returnCallback(
+                    \Closure::bind(
+                        function(InputInterface $input, OutputInterface $output, Question $question){
+                            $this->assertSame(
+                                '<question>Enter the author (name <mail@mail.com>):</question>',
+                                $question->getQuestion()
+                            );
+                            return new Author('main.name <main.email@mail.com>');
+                        },
+                        $this
+                    )
+                )
+            );
+
+        $questionHelper->expects($this->at(1))
+            ->method('ask')
+            ->will(
+                $this->returnCallback(
+                    \Closure::bind(
+                        function(InputInterface $input, OutputInterface $output, Question $question){
+                            $this->assertSame(
+                                '<question>Do you want to add another author?[n]</question>',
+                                $question->getQuestion()
+                            );
+                            return true;
+                        },
+                        $this
+                    )
+                )
+            );
+
+        $questionHelper->expects($this->at(2))
+            ->method('ask')
+            ->will(
+                $this->returnCallback(
+                    \Closure::bind(
+                        function(InputInterface $input, OutputInterface $output, Question $question){
+                            $this->assertSame(
+                                '<question>Enter the author (name <mail@mail.com>):</question>',
+                                $question->getQuestion()
+                            );
+                            return new Author('add.name <add.email@mail.com>');
+                        },
+                        $this
+                    )
+                )
+            );
+
+        $services['question'] = function () use ($questionHelper){
+            return $questionHelper;
+        };
+
+        return $services;
+    }
+
+}
