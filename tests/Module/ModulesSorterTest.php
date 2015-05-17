fa3eda37d85ffb03259abc2d54a9351115583880
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2015-05-17 22:49:46 +0200

    create ModulesSorter

---
+++ tests/Module/ModulesSorterTest.php
@@ -0,0 +1,93 @@
+<?php
+namespace Samurai\Module;
+
+/**
+ * Class ModulesSorterTest
+ * @package Samurai\Module\Task
+ * @author Raphaël Lefebvre <raphael@raphaellefebvre.be>
+ */
+class ModulesSorterTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testSort()
+    {
+        $modules = new Modules();
+
+        $moduleA = new Module();
+        $moduleA->setPackage('a');
+
+        $moduleB = new Module();
+        $moduleB->setPackage('b');
+
+        $moduleC = new Module();
+        $moduleC->setPackage('c');
+
+        $moduleB->setDependencies(new Modules([$moduleC, $moduleA]));
+        $moduleC->setDependencies(new Modules([$moduleA]));
+
+        $modules['a'] = $moduleA;
+        $modules['b'] = $moduleB;
+        $modules['c'] = $moduleC;
+
+        $sorter = new ModulesSorter();
+        $result = $sorter->sort($modules);
+        $this->assertSame(
+            ['a', 'c', 'b'],
+            array_keys($result->getArrayCopy())
+        );
+    }
+
+    public function testSortNoDependencies()
+    {
+        $modules = new Modules();
+
+        $moduleA = new Module();
+        $moduleA->setPackage('a');
+
+        $moduleB = new Module();
+        $moduleB->setPackage('b');
+
+        $moduleC = new Module();
+        $moduleC->setPackage('c');
+
+        $modules['a'] = $moduleA;
+        $modules['b'] = $moduleB;
+        $modules['c'] = $moduleC;
+
+        $sorter = new ModulesSorter();
+        $result = $sorter->sort($modules);
+        $this->assertSame(
+            ['a', 'b', 'c'],
+            array_keys($result->getArrayCopy())
+        );
+    }
+
+    public function testSortRecursiveDependencies()
+    {
+        $modules = new Modules();
+
+        $moduleA = new Module();
+        $moduleA->setPackage('a');
+
+        $moduleB = new Module();
+        $moduleB->setPackage('b');
+
+        $moduleC = new Module();
+        $moduleC->setPackage('c');
+
+        $moduleB->setDependencies(new Modules([$moduleC]));
+        $moduleC->setDependencies(new Modules([$moduleB]));
+
+        $modules['a'] = $moduleA;
+        $modules['b'] = $moduleB;
+        $modules['c'] = $moduleC;
+
+        $sorter = new ModulesSorter();
+
+        $this->setExpectedException(
+            'RuntimeException',
+            'Modules sort not possible. Maybe recursive dependencies in these modules: b, c.'
+        );
+        $sorter->sort($modules);
+    }
+}
