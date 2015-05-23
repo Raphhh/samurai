43c8a015122014d0312aa6114039c55bad78b39e
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2015-05-23 17:32:01 +0200

    create Removing

---
+++ src/Module/Task/Removing.php
@@ -0,0 +1,65 @@
+<?php
+namespace Samurai\Module\Task;
+
+use Samurai\Task\ITask;
+use Samurai\Task\Task;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+
+/**
+ * Class Removing
+ * @package Samurai\module\Task
+ * @author Raphaël Lefebvre <raphael@raphaellefebvre.be>
+ */
+class Removing extends Task
+{
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return bool
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $moduleName = $input->getArgument('name');
+        if(!$this->getService('module_manager')->has($moduleName)){
+            $output->writeln(sprintf('<error>Error: no module "%s" found</error>', $moduleName));
+            return ITask::BLOCKING_ERROR_CODE;
+        }
+
+        if($this->confirmRemove($input, $output, $moduleName)){
+            $this->getService('module_importer')->setOutput($output);
+            $this->getService('module_importer')->remove($this->getService('module_manager')->get($moduleName));
+        }
+        return ITask::NO_ERROR_CODE;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @param $moduleName
+     * @return bool
+     */
+    private function confirmRemove(InputInterface $input, OutputInterface $output, $moduleName)
+    {
+        return $this->getService('helper_set')->get('question')->ask(
+            $input,
+            $output,
+            $this->buildQuestion($moduleName)
+        );
+    }
+
+    /**
+     * @param string $moduleName
+     * @return ConfirmationQuestion
+     */
+    private function buildQuestion($moduleName)
+    {
+        return new ConfirmationQuestion(
+            sprintf(
+                '<question>Do you want to remove the module "%s"</question>[y]',
+                $moduleName
+            )
+        );
+    }
+}
---
+++ tests/Module/Task/RemovingTest.php
@@ -0,0 +1,236 @@
+<?php
+namespace Samurai\Module\Task;
+
+use Pimple\Container;
+use Samurai\Module\Module;
+use Samurai\Task\ITask;
+use Symfony\Component\Console\Helper\HelperSet;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+
+/**
+ * Class RemovingTest
+ * @package Samurai\Module\Task
+ * @author Raphaël Lefebvre <raphael@raphaellefebvre.be>
+ */
+class RemovingTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testExecuteWithoutModule()
+    {
+        $args = [
+            'name' => 'name',
+            'description' => 'description',
+            'bootstrap' => 'bootstrap',
+            'version' => 'version',
+        ];
+        $input = $this->provideInput(['name' => 'name']);
+        $output = new BufferedOutput();
+
+        $saving = new Removing(
+            $this->provideServices(
+                $this->provideModule($args),
+                false,
+                false,
+                $input,
+                $output
+            )
+        );
+        $this->assertSame(ITask::BLOCKING_ERROR_CODE, $saving->execute($input, $output));
+        $this->assertSame("Error: no module \"name\" found\n", $output->fetch());
+    }
+
+    public function testExecuteWithModuleAndOverride()
+    {
+        $args = [
+            'name' => 'name',
+            'description' => 'description',
+            'bootstrap' => 'bootstrap',
+            'version' => 'version',
+        ];
+        $input = $this->provideInput(['name' => 'name']);
+        $output = new BufferedOutput();
+
+        $saving = new Removing(
+            $this->provideServices(
+                $this->provideModule($args),
+                true,
+                true,
+                $input,
+                $output
+            )
+        );
+        $this->assertSame(ITask::NO_ERROR_CODE, $saving->execute($input, $output));
+        $this->assertSame("", $output->fetch());
+    }
+
+    public function testExecuteWithModuleButNoOverride()
+    {
+        $args = [
+            'name' => 'name',
+            'description' => 'description',
+            'bootstrap' => 'bootstrap',
+            'version' => 'version',
+        ];
+        $input = $this->provideInput(['name' => 'name']);
+        $output = new BufferedOutput();
+
+        $saving = new Removing(
+            $this->provideServices(
+                $this->provideModule($args),
+                true,
+                false,
+                $input,
+                $output
+            )
+        );
+        $this->assertSame(ITask::NO_ERROR_CODE, $saving->execute($input, $output));
+        $this->assertSame("", $output->fetch());
+    }
+
+    /**
+     * @param array $args
+     * @return ArrayInput
+     */
+    private function provideInput(array $args)
+    {
+        return new ArrayInput(
+            $args,
+            new InputDefinition([
+                new InputArgument('name'),
+                new InputArgument('description'),
+                new InputArgument('bootstrap'),
+                new InputArgument('version'),
+            ])
+        );
+    }
+
+    /**
+     * @param Module $module
+     * @param bool $hasModule
+     * @param bool $willBeRemoved
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return Container
+     */
+    private function provideServices(Module $module, $hasModule, $willBeRemoved, InputInterface $input, OutputInterface $output)
+    {
+        $services = new Container();
+
+        $moduleManager = $this->provideModuleManager($module, $hasModule, $willBeRemoved);
+        $services['module_manager'] = function () use($moduleManager){
+            return $moduleManager;
+        };
+
+        $moduleImporter = $this->provideModuleImporter($module, $hasModule, $willBeRemoved);
+        $services['module_importer'] = function () use($moduleImporter){
+            return $moduleImporter;
+        };
+
+        if($hasModule) {
+            $questionHelper = $this->provideQuestionHelper($input, $output, $willBeRemoved);
+            $services['helper_set'] = function () use ($questionHelper) {
+                return new HelperSet(['question' => $questionHelper]);
+            };
+        }
+
+        return $services;
+    }
+
+    /**
+     * @param array $args
+     * @return Module
+     */
+    private function provideModule(array $args)
+    {
+        $module = new Module();
+        $module->setName($args['name']);
+        $module->setDescription($args['description']);
+        $module->setPackage($args['bootstrap']);
+        $module->setVersion($args['version']);
+        return $module;
+    }
+
+    /**
+     * @param Module $module
+     * @param $hasModule
+     * @param $willBeRemoved
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function provideModuleManager(Module $module, $hasModule, $willBeRemoved)
+    {
+        $moduleManager = $this->getMockBuilder('Samurai\Module\ModuleManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $moduleManager->expects($this->once())
+            ->method('has')
+            ->with($module->getName())
+            ->will($this->returnValue($hasModule));
+
+        if($hasModule && $willBeRemoved) {
+            $moduleManager->expects($this->once())
+                ->method('get')
+                ->will($this->returnValue($module));
+        }else{
+            $moduleManager->expects($this->never())
+                ->method('get');
+        }
+
+        return $moduleManager;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @param bool $result
+     * @return \Symfony\Component\Console\Helper\QuestionHelper
+     */
+    private function provideQuestionHelper(InputInterface $input, OutputInterface $output, $result)
+    {
+        $questionHelper = $this->getMockBuilder('Symfony\Component\Console\Helper\QuestionHelper')->getMock();
+
+        $questionHelper->expects($this->once())
+            ->method('ask')
+            ->with(
+                $input,
+                $output,
+                $this->callback(
+                    function (ConfirmationQuestion $question) {
+                        return '<question>Do you want to remove the module "name"</question>[y]' === $question->getQuestion();
+                    }
+                )
+            )
+            ->will($this->returnValue($result));
+
+        return $questionHelper;
+    }
+
+    /**
+     * @param Module $module
+     * @param $hasModule
+     * @param $willBeRemoved
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function provideModuleImporter(Module $module, $hasModule, $willBeRemoved)
+    {
+        $moduleImporter = $this->getMockBuilder('Samurai\Module\ModuleImporter')->disableOriginalConstructor()->getMock();
+
+        if($hasModule && $willBeRemoved){
+            $moduleImporter->expects($this->once())
+                ->method('remove')
+                ->with($module)
+                ->will($this->returnValue(true));
+        }else{
+            $moduleImporter->expects($this->never())
+                ->method('remove');
+        }
+
+        return $moduleImporter;
+    }
+}
