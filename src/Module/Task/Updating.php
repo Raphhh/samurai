4a8ad4a6314d3e25c884555fce4bce78feb67083
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2015-05-23 21:48:57 +0200

    create Updating

---
+++ src/Module/Task/Updating.php
@@ -0,0 +1,34 @@
+<?php
+namespace Samurai\Module\Task;
+
+use Samurai\Task\ITask;
+use Samurai\Task\Task;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class Updating
+ * @package Samurai\Module\Task
+ * @author Raphaël Lefebvre <raphael@raphaellefebvre.be>
+ */
+class Updating extends Task
+{
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|null
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $moduleName = $input->getArgument('name');
+        if(!$this->getService('module_manager')->has($moduleName)){
+            $output->writeln(sprintf('<error>Error: no module "%s" found</error>', $moduleName));
+            return ITask::BLOCKING_ERROR_CODE;
+        }
+
+        $this->getService('module_procedure')->setOutput($output);
+        $this->getService('module_procedure')->update($this->getService('module_manager')->get($moduleName));
+        return ITask::NO_ERROR_CODE;
+    }
+}
---
+++ tests/Module/Task/UpdatingTest.php
@@ -0,0 +1,162 @@
+<?php
+namespace Samurai\Module\Task;
+
+use Pimple\Container;
+use Samurai\Module\Module;
+use Samurai\Task\ITask;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+/**
+ * Class UpdatingTest
+ * @package Samurai\Module\Task
+ * @author Raphaël Lefebvre <raphael@raphaellefebvre.be>
+ */
+class UpdatingTest extends \PHPUnit_Framework_TestCase
+{
+    public function testExecuteWithModule()
+    {
+        $args = [
+            'name' => 'name',
+            'description' => 'description',
+            'bootstrap' => 'bootstrap',
+            'version' => 'version',
+        ];
+        $input = $this->provideInput(['name' => 'name']);
+        $output = new BufferedOutput();
+
+        $updating = new Updating(
+            $this->provideServices(
+                $this->provideModule($args),
+                true
+            )
+        );
+        $this->assertSame(ITask::NO_ERROR_CODE, $updating->execute($input, $output));
+        $this->assertSame("", $output->fetch());
+    }
+
+    public function testExecuteWithoutModule()
+    {
+        $args = [
+            'name' => 'name',
+            'description' => 'description',
+            'bootstrap' => 'bootstrap',
+            'version' => 'version',
+        ];
+        $input = $this->provideInput(['name' => 'name']);
+        $output = new BufferedOutput();
+
+        $updating = new Updating(
+            $this->provideServices(
+                $this->provideModule($args),
+                false
+            )
+        );
+        $this->assertSame(ITask::BLOCKING_ERROR_CODE, $updating->execute($input, $output));
+        $this->assertSame("Error: no module \"name\" found\n", $output->fetch());
+    }
+
+    /**
+     * @param array $args
+     * @return ArrayInput
+     */
+    private function provideInput(array $args)
+    {
+        return new ArrayInput(
+            $args,
+            new InputDefinition([
+                new InputArgument('name'),
+            ])
+        );
+    }
+
+    /**
+     * @param Module $module
+     * @param bool $hasModule
+
+     * @return Container
+     */
+    private function provideServices(Module $module, $hasModule)
+    {
+        $services = new Container();
+
+        $moduleManager = $this->provideModuleManager($module, $hasModule);
+        $services['module_manager'] = function () use($moduleManager){
+            return $moduleManager;
+        };
+
+        $moduleProcedure = $this->provideModuleProcedure($module, $hasModule);
+        $services['module_procedure'] = function () use($moduleProcedure){
+            return $moduleProcedure;
+        };
+
+
+        return $services;
+    }
+
+    /**
+     * @param array $args
+     * @return Module
+     */
+    private function provideModule(array $args)
+    {
+        $module = new Module();
+        $module->setName($args['name']);
+        $module->setDescription($args['description']);
+        $module->setPackage($args['bootstrap']);
+        $module->setVersion($args['version']);
+        return $module;
+    }
+
+    /**
+     * @param Module $module
+     * @param $hasModule
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function provideModuleManager(Module $module, $hasModule)
+    {
+        $moduleManager = $this->getMockBuilder('Samurai\Module\ModuleManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $moduleManager->expects($this->once())
+            ->method('has')
+            ->with($module->getName())
+            ->will($this->returnValue($hasModule));
+
+        if($hasModule) {
+            $moduleManager->expects($this->once())
+                ->method('get')
+                ->will($this->returnValue($module));
+        }else{
+            $moduleManager->expects($this->never())
+                ->method('get');
+        }
+
+        return $moduleManager;
+    }
+
+    /**
+     * @param Module $module
+     * @param $hasModule
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function provideModuleProcedure(Module $module, $hasModule)
+    {
+        $moduleProcedure = $this->getMockBuilder('Samurai\Module\ModuleProcedure')->disableOriginalConstructor()->getMock();
+
+        if($hasModule){
+            $moduleProcedure->expects($this->once())
+                ->method('update')
+                ->with($module)
+                ->will($this->returnValue(true));
+        }else{
+            $moduleProcedure->expects($this->never())
+                ->method('update');
+        }
+
+        return $moduleProcedure;
+    }
+}
